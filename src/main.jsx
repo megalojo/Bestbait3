ec7aca9bb60285259d23a9c8f9e7082c9c1ed3bc
megalojo <bruzz.paul@gmail.com>
2026-05-13 20:06:30 -0400

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import BestBaitApp from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BestBaitApp />
+  </React.StrictMode>
+)
